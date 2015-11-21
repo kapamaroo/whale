4b89efd9d78a5e7acf205d66ed8f18305a537c14
Maroudas Manolis <kapamaroo@gmail.com>
2015-11-21 14:58:38 +0200

    add comments about the implementation of WhaleVector

--- include/whale/WhaleVector.h
+++ include/whale/WhaleVector.h
@@ -2,6 +2,15 @@
 #define __WHALE_VECTOR_H__
 
 #include <whale/WhaleCore.h>
+
+/* Example implementation
+
+   Use the internal WhaleVersion of DOLFIN, to demonstrate how to implement
+   the Whale library. In a real implementation, instead of using directly this
+   internal header, one may implement a Vector structure in a separate header
+   and use that in to develop this library. In this case, the internal header
+   must be updated to use the aforementioned Vector structure.
+ */
 #include <dolfin/la/WhaleVector.h>
 
 namespace whale {
